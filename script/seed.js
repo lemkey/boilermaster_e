8c6932c82d8f54fbd93eadb16448450b9db0f065
Erin Lemkey <elemkey@gmail.com>
2021-06-07 16:48:50 -0400

    changed one import

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const {db} = require('../server/db')
-const {User} = require('../server/db/models/user')
+const {User} = require('../server/db')
 
 /**
  * seed - this function clears the database, updates tables to
